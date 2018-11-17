3900308e89dcca6e2ad6066f281182db311797e4
Arne Johannessen <git@arne.johannessen.de>
2018-11-18 00:34:33 +0100

    Deprecate get_bool()
    The various JSON modules for Perl typically represent a boolean false value using a blessed scalar overloaded to evaluate to false in Perl expressions. This almost always works perfectly fine. However, some client tests don't expect a non-truthy value to be blessed, which can result in wrong interpretation of query results. The get_bool() method was meant to ensure boolean results would evaluate correctly in such cases. Given that such cases are rare and that no specific examples for such cases are currently known, this method now seems superfluous.

--- lib/Neo4j/Driver/Record.pm
+++ lib/Neo4j/Driver/Record.pm
@@ -7,7 +7,7 @@ package Neo4j::Driver::Record;
 # ABSTRACT: container for Cypher result values
 
 
-use Carp qw(croak);
+use Carp qw(carp croak);
 use JSON::PP;
 
 
@@ -21,8 +21,17 @@ sub get {
 }
 
 
+# The various JSON modules for Perl tend to represent a boolean false value
+# using a blessed scalar overloaded to evaluate to false in Perl expressions.
+# This almost always works perfectly fine. However, some tests might not expect
+# a non-truthy value to be blessed, which can result in wrong interpretation of
+# query results. The get_bool method was meant to ensure boolean results would
+# evaluate correctly in such cases. Given that such cases are rare and that no
+# specific examples for such cases are currently known, this method now seems
+# superfluous.
 sub get_bool {
 	my ($self, $field) = @_;
+	carp __PACKAGE__ . "->get_bool is deprecated";
 	
 	my $value = $self->get($field);
 	return $value if ! ref $value;
