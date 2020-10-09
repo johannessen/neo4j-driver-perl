33125f20bca276647c5c7fd3c755acedc5468759
Arne Johannessen <git@arne.johannessen.de>
2020-10-09 18:46:48 +0200

    Improve compatibility with outdated JSON module versions
    Try to anyway … not sure if it helps!

--- lib/Neo4j/Driver/Transport/HTTP.pm
+++ lib/Neo4j/Driver/Transport/HTTP.pm
@@ -13,7 +13,7 @@ use Try::Tiny;
 
 use URI 1.25;
 use REST::Client 134;
-use JSON::MaybeXS 1.002004 qw(JSON);
+use JSON::MaybeXS 1.003003 qw();
 
 use Neo4j::Driver::ResultSummary;
 use Neo4j::Driver::StatementResult;
@@ -114,7 +114,7 @@ sub prepare {
 	my $json = { statement => '' . $query };
 	$json->{resultDataContents} = $RESULT_DATA_CONTENTS;
 	$json->{resultDataContents} = $RESULT_DATA_CONTENTS_GRAPH if $self->{return_graph};
-	$json->{includeStats} = JSON()->true if $self->{return_stats};
+	$json->{includeStats} = \1 if $self->{return_stats};
 	$json->{parameters} = $parameters if defined $parameters;
 	
 	return $json;
