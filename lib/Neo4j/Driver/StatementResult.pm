251de6da3b62e53600844236d36a54eb384a95c3
Arne Johannessen <noreply@arne.johannessen.de>
2024-11-14 16:44:34 +0100

    Remove outdated documentation from CPAN dist

--- lib/Neo4j/Driver/StatementResult.pm
+++ lib/Neo4j/Driver/StatementResult.pm
@@ -10,13 +10,6 @@ package Neo4j::Driver::StatementResult;
 
 __END__
 
-=head1 SYNOPSIS
-
- package Neo4j::Driver::Result;
- use parent 'Neo4j::Driver::StatementResult';
-
-=head1 DESCRIPTION
-
 The Neo4j::Driver::StatementResult module was renamed to
 L<Neo4j::Driver::Result> for version 0.19.
 
@@ -25,28 +18,3 @@ module name was used only internally in the driver. Objects were
 created by calling methods on session or transaction objects;
 a C<new()> method was never publicly exposed for this module.
 Therefore no issues due to this change are expected.
-
-However, it is legal for clients to verify the type of result
-objects using L<< isa()|UNIVERSAL/$obj->isa(-TYPE-) >>. To keep
-such existing checks working for the time being, a module with
-this name continues to be provided for use as a parent for
-L<Neo4j::Driver::Result>.
-
-B<Any use of C<Neo4j::Driver::StatementResult> is deprecated.>
-
-This module will be removed in a future version of this driver.
-
-=head1 SEE ALSO
-
-=over
-
-=item * L<Neo4j::Driver>
-
-=item * L<Neo4j::Driver::B<Result>>
-
-=item * Naming changes for the Neo4j Java driver 4.0:
-L<neo4j/neo4j-java-driver#651|https://github.com/neo4j/neo4j-java-driver/pull/651>
-
-=back
-
-=cut
--- lib/Neo4j/Driver/Type/Temporal.pm
+++ lib/Neo4j/Driver/Type/Temporal.pm
@@ -10,16 +10,6 @@ package Neo4j::Driver::Type::Temporal;
 
 __END__
 
-=head1 SYNOPSIS
-
- package Neo4j::Driver::Type::DateTime;
- use parent 'Neo4j::Driver::Type::Temporal';
- 
- package Neo4j::Driver::Type::Duration;
- use parent 'Neo4j::Driver::Type::Temporal';
-
-=head1 DESCRIPTION
-
 The Neo4j::Driver::Type::Temporal module was created to
 mark temporal values returned from the Neo4j server.
 However, beyond that, it was never implemented, and the
@@ -31,15 +21,3 @@ in future.
 Since the L<Neo4j::Types> model doesn't have a shared super-type
 for temporal instants and temporal durations, this module is
 no longer required.
-
-B<Any use of C<Neo4j::Driver::Type::Temporal> is deprecated.>
-
-This module will be removed in a future version of this driver.
-
-=head1 SEE ALSO
-
-=over
-
-=item * L<Neo4j::Driver::Types>
-
-=back
