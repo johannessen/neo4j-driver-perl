johannessen/neo4j-driver-perl

commit ccfd67184be0e2bea26cf1bbfd87256cfb78b9e5

Replace Try::Tiny with Feature::Compat::Try

lib/Neo4j/Driver/Session.pm
@@ -1,4 +1,4 @@
-use v5.12;
+use v5.14;
 use warnings;
 
 package Neo4j::Driver::Session;
@@ -8,12 +8,11 @@ package Neo4j::Driver::Session;
 use Carp qw(croak);
 our @CARP_NOT = qw(
 	Neo4j::Driver
-	Try::Tiny
 );
+use Feature::Compat::Try;
 use List::Util qw(min);
 use Scalar::Util qw(blessed);
 use Time::HiRes ();
-use Try::Tiny;
 use URI 1.25;
 
 use Neo4j::Driver::Net::Bolt;
@@ -66,53 +65,45 @@ sub _execute {
 	
 	$self->{retry_sleep} //= 1;
 	my (@r, $r);
-	my $wantarray = wantarray;
 	my $time_stop = Time::HiRes::time
 		+ ($self->{driver}->config('max_transaction_retry_time') // 30);  # seconds
 	my $tries = 0;
-	my $success = 0;
-	do {
+	while () {
 		my $tx = $self->new_tx($mode);
 		$tx->{error_handler} = sub { die shift };
 		
 		try {
 			$tx->_begin;
 			$tx->{managed} = 1;  # Disallow commit() in $func
-			if ($wantarray) {
-				@r = $func->($tx);
-			}
-			else {
-				$r = $func->($tx);
-			}
+			wantarray ? @r = $func->($tx) : $r = $func->($tx);
 			$tx->{managed} = 0;
 			$tx->commit;
-			$success = 1;  # return from sub not possible in a Try::Tiny block
+			
+			return unless defined wantarray;
+			$r = $r[0] if wantarray;
+			warnings::warnif closure => "Result object may not be valid outside the transaction function"
+				if blessed $r && $r->isa('Neo4j::Driver::Result');
+			
+			return wantarray ? @r : $r;
 		}
-		catch {
+		catch ($e) {
 			# The tx may or may not already be closed; we need to make sure
 			$tx->{managed} = 0;
-			try { $tx->rollback };
+			try { $tx->rollback } catch ($f) {}
 			
 			# Never retry non-Neo4j errors
-			croak $_ unless blessed $_ && $_->isa('Neo4j::Error');
+			croak $e unless blessed $e && $e->isa('Neo4j::Error');
 			
-			if (! $_->is_retryable || Time::HiRes::time >= $time_stop) {
-				$self->{driver}->{plugins}->trigger( error => $_ );
-				$success = -1;  # return in case the event handler doesn't die
+			if (! $e->is_retryable || Time::HiRes::time >= $time_stop) {
+				$self->{driver}->{plugins}->trigger( error => $e );
+				return;  # in case the event handler doesn't die
 			}
-			else {
-				Time::HiRes::sleep min
-					$self->{retry_sleep} * (1 << $tries++),
-					$time_stop - Time::HiRes::time;
-			}
-		};
-	} until ($success);
-	
-	$r = $r[0] if $wantarray;
-	if (defined $wantarray && blessed $r && $r->isa('Neo4j::Driver::Result')) {
-		warnings::warnif closure => "Result object may not be valid outside the transaction function";
+		}
+		
+		Time::HiRes::sleep min
+			$self->{retry_sleep} * (1 << $tries++),
+			$time_stop - Time::HiRes::time;
 	}
-	return $wantarray ? @r : $r;
 }
 
 

dist.ini
@@ -6,7 +6,7 @@ license = Artistic_2_0
 copyright_holder = Arne Johannessen
 copyright_year   = 2016-2024
 
-version = 1.0061
+version = 1.0062
 release_status = unstable
 
 [Meta::Contributors]

lib/Neo4j/Driver/Net/Bolt.pm
@@ -1,4 +1,4 @@
-use v5.12;
+use v5.14;
 use warnings;
 
 package Neo4j::Driver::Net::Bolt;
@@ -10,8 +10,7 @@ package Neo4j::Driver::Net::Bolt;
 
 use Carp qw(croak);
 our @CARP_NOT = qw(Neo4j::Driver::Transaction Neo4j::Driver::Transaction::Bolt);
-
-use Try::Tiny;
+use Feature::Compat::Try;
 use URI 1.25;
 
 use Neo4j::Driver::Result::Bolt;
@@ -87,11 +86,10 @@ sub _verify_version {
 		$client_version =~ m/^0\.5[012]$/ and die
 			sprintf "Installed Neo4j::Client version %s is defective (known-good versions are 0.46 and 0.54 or later; you may also need to reinstall Neo4j::Bolt)\n", $client_version;
 	}
-	catch {
-		my $e = $_;
+	catch ($e) {
 		$e =~ s/\.?\s*$//;
 		croak sprintf "Protocol scheme 'bolt' is not supported (Neo4j::Bolt not installed).\nNeo4j::Driver will support 'bolt' URLs if the Neo4j::Bolt module is installed.\n%s", $e;
-	};
+	}
 }
 
 
@@ -108,19 +106,19 @@ sub _trigger_bolt_error {
 		code => scalar $ref->server_errcode,
 		message => scalar $ref->server_errmsg,
 		raw => scalar $ref->get_failure_details,
-	}) if try { $ref->server_errcode || $ref->server_errmsg };
+	}) if eval { $ref->server_errcode || $ref->server_errmsg };
 	
 	$error = $error->append_new( Network => {
 		code => scalar $ref->client_errnum,
 		message => scalar $ref->client_errmsg,
 		as_string => $self->_bolt_error($ref),
-	}) if try { $ref->client_errnum || $ref->client_errmsg };
+	}) if eval { $ref->client_errnum || $ref->client_errmsg };
 	
 	$error = $error->append_new( Network => {
 		code => scalar $ref->errnum,
 		message => scalar $ref->errmsg,
 		as_string => $self->_bolt_error($ref),
-	}) if try { $ref->errnum || $ref->errmsg };
+	}) if eval { $ref->errnum || $ref->errmsg };
 	
 	try {
 		my $cxn = $connection // $self->{connection};
@@ -128,14 +126,15 @@ sub _trigger_bolt_error {
 			code => scalar $cxn->errnum,
 			message => scalar $cxn->errmsg,
 			as_string => $self->_bolt_error($cxn),
-		}) if try { $cxn->errnum || $cxn->errmsg } && $cxn != $ref;
+		}) if eval { $cxn->errnum || $cxn->errmsg } && $cxn != $ref;
 		$cxn->reset_cxn;
 		$error = $error->append_new( Internal => {  # perlbolt#51
 			code => scalar $cxn->errnum,
 			message => scalar $cxn->errmsg,
 			as_string => $self->_bolt_error($cxn),
-		}) if try { $cxn->errnum || $cxn->errmsg };
-	};
+		}) if eval { $cxn->errnum || $cxn->errmsg };
+	}
+	catch ($e) {}
 	
 	return $error_handler->($error) if ref $error_handler eq 'CODE';
 	$error_handler->trigger(error => $error);

lib/Neo4j/Driver/Result/JSON.pm
@@ -1,4 +1,4 @@
-use v5.12;
+use v5.14;
 use warnings;
 
 package Neo4j::Driver::Result::JSON;
@@ -12,9 +12,8 @@ use parent 'Neo4j::Driver::Result';
 
 use Carp qw(carp croak);
 our @CARP_NOT = qw(Neo4j::Driver::Net::HTTP);
+use Feature::Compat::Try;
 use JSON::MaybeXS 1.002004 ();
-use Try::Tiny;
-
 use URI 1.31;
 
 use Neo4j::Error;
@@ -87,12 +86,12 @@ sub _parse_json {
 	try {
 		$json = $params->{http_agent}->json_coder->decode($response);
 	}
-	catch {
+	catch ($e) {
 		$error = $error->append_new( Internal => {
-			as_string => "$_",
+			as_string => "$e",
 			raw => $response,
 		});
-	};
+	}
 	if (ref $json->{errors} eq 'ARRAY') {
 		$error = $error->append_new( Server => $_ ) for @{$json->{errors}};
 	}

lib/Neo4j/Driver/ServerInfo.pm
@@ -1,4 +1,4 @@
-use v5.12;
+use v5.14;
 use warnings;
 
 package Neo4j::Driver::ServerInfo;
@@ -7,6 +7,7 @@ package Neo4j::Driver::ServerInfo;
 
 use Carp qw(croak);
 our @CARP_NOT = qw(Neo4j::Driver::Session);
+use Feature::Compat::Try;
 use URI 1.25;
 
 
@@ -45,14 +46,14 @@ sub _default_database {
 	return $database if defined $database;
 	
 	return if $self->{version} =~ m{^Neo4j/[123]\.};
-	eval {
+	try {
 		my $sys = $driver->session(database => 'system');
 		$database = $sys->run('SHOW DEFAULT DATABASE')->single->get('name');
-	};
-	unless (defined $database) {
+	}
+	catch ($e) {
 		croak sprintf
 			"%sSession creation failed because the default database of %s at %s could not be determined",
-			$@, $self->{version}, $self->{uri};
+			$e, $self->{version}, $self->{uri};
 	}
 	return $self->{default_database} = $database;
 }

lib/Neo4j/Driver/Transaction.pm
@@ -1,4 +1,4 @@
-use v5.12;
+use v5.14;
 use warnings;
 
 package Neo4j::Driver::Transaction;
@@ -10,7 +10,6 @@ our @CARP_NOT = qw(
 	Neo4j::Driver::Session
 	Neo4j::Driver::Session::Bolt
 	Neo4j::Driver::Session::HTTP
-	Try::Tiny
 );
 use Scalar::Util qw(blessed);
 
@@ -99,7 +98,7 @@ package # private
 use parent -norequire => 'Neo4j::Driver::Transaction';
 
 use Carp qw(croak);
-use Try::Tiny;
+use Feature::Compat::Try;
 
 
 sub _begin {
@@ -110,9 +109,9 @@ sub _begin {
 	try {
 		$self->{bolt_txn} = $self->{net}->_new_tx($self);
 	}
-	catch {
-		die $_ if $_ !~ m/\bprotocol version\b/i;  # Bolt v1/v2
-	};
+	catch ($e) {
+		die $e if $e !~ m/\bprotocol version\b/i;  # Bolt v1/v2
+	}
 	$self->{net}->{active_tx} = 1;
 	$self->run('BEGIN') unless $self->{bolt_txn};
 	return $self;
@@ -129,10 +128,10 @@ sub _run_autocommit {
 	try {
 		$results = $self->run($query, @parameters);
 	}
-	catch {
+	catch ($e) {
 		$self->{net}->{active_tx} = 0;
-		croak $_;
-	};
+		croak $e;
+	}
 	$self->{net}->{active_tx} = 0;
 	
 	return $results;

t/config.t
@@ -1,5 +1,5 @@
 #!perl
-use strict;
+use v5.14;
 use warnings;
 use lib qw(./lib t/lib);
 
@@ -16,7 +16,7 @@ use if $no_warnings = $ENV{AUTHOR_TESTING} ? 1 : 0, 'Test::Warnings';
 use Neo4j_Test;
 use Neo4j_Test::MockHTTP;
 
-use Try::Tiny;
+use Feature::Compat::Try;
 
 my ($d, $r, $w);
 
@@ -236,7 +236,7 @@ subtest 'neo4j uri scheme' => sub {
 		$d = Neo4j::Driver->new('neo4j://bb');
 		my $is_bolt;
 		try { $d->session }
-		catch { $is_bolt = m<\bNeo4j::Bolt\b.* at lib/Neo4j/Driver/Net/Bolt\.pm\b> };
+		catch ($e) { $is_bolt = $e =~ m<\bNeo4j::Bolt\b.* at lib/Neo4j/Driver/Net/Bolt\.pm\b> }
 		ok $is_bolt && $d->{config}{uri} eq 'bolt://bb:7687';
 	} 'neo4j bolt uri default port';
 	lives_and {
@@ -245,7 +245,7 @@ subtest 'neo4j uri scheme' => sub {
 		$d = Neo4j::Driver->new('neo4j://bbp:80');
 		my $is_bolt;
 		try { $d->session }
-		catch { $is_bolt = m<\bNeo4j::Bolt\b.* at lib/Neo4j/Driver/Net/Bolt\.pm\b> };
+		catch ($e) { $is_bolt = $e =~ m<\bNeo4j::Bolt\b.* at lib/Neo4j/Driver/Net/Bolt\.pm\b> }
 		ok $is_bolt && $d->{config}{uri} eq 'bolt://bbp:80';
 	} 'neo4j bolt uri explicit port';
 };

t/lib/Neo4j_Test.pm
@@ -1,7 +1,8 @@
 package Neo4j_Test;
-use strict;
+use v5.14;
 use warnings;
 
+use Feature::Compat::Try;
 use URI;
 use Neo4j::Driver;
 use Neo4j_Test::NetModulePlugin;
@@ -22,11 +23,11 @@ our $sim;
 sub driver_maybe {
 	
 	my $driver;
-	eval {
+	try {
 		# a default URI (localhost) is built into the driver
 		$driver = Neo4j::Driver->new( $server );
-	};
-	return unless $driver;
+	}
+	catch ($e) { return }
 	
 	$driver->basic_auth($user, $pass);
 	$driver->config(timeout => 2);  # 2 seconds timeout may speed up testing
@@ -52,12 +53,12 @@ sub driver {
 	my $driver = driver_maybe;
 	
 	# verify that the supplied credentials actually work
-	eval {
+	try {
 		# the Neo4j HTTP API allows running empty statements
 		$driver->session->run('');
-	};
-	if ($@) {
-		$error = $@;
+	}
+	catch ($e) {
+		$error = $e;
 		return;
 	}
 	

commit e68cf52eb72e3ca94eb2a15be4749011cc1a69b4

Avoid unneeded executions of managed tx functions
Perl's ternary operator binds more tightly than assignment, so parentheses are necessary here. Before this change, tx functions were executed twice when called in list context. Because the managed transaction logic may execute tx functions multiple times, they always need to be idempotent anyway. So managed transactions still worked correctly; it was just a performance issue.

This was a regression introduced recently in ccfd67184be0e2bea26cf1bbfd87256cfb78b9e5, later released as 1.01-TRIAL.

lib/Neo4j/Driver/Session.pm
@@ -63,7 +63,6 @@ sub _execute {
 	croak sprintf "%s->execute_%s() requires subroutine ref", __PACKAGE__, lc $mode unless ref $func eq 'CODE';
 	
 	$self->{retry_sleep} //= 1;
-	my (@r, $r);
 	my $time_stop = Time::HiRes::time
 		+ ($self->{driver}->config('max_transaction_retry_time') // 30);  # seconds
 	my $tries = 0;
@@ -74,7 +73,7 @@ sub _execute {
 		try {
 			$tx->_begin;
 			$tx->{managed} = 1;  # Disallow commit() in $func
-			wantarray ? @r = $func->($tx) : $r = $func->($tx);
+			wantarray ? (my @r = $func->($tx)) : (my $r = $func->($tx));
 			$tx->{managed} = 0;
 			$tx->commit;
 			

t/session-execute.t
@@ -22,7 +22,7 @@ use Neo4j_Test;
 use Neo4j_Test::EchoHTTP;
 use Neo4j_Test::MockQuery;
 
-plan tests => 9 + $no_warnings;
+plan tests => 10 + $no_warnings;
 
 
 my ($d, $s, $r, @r);
@@ -154,6 +154,17 @@ subtest 'commit/rollback' => sub {
 	})} 'rollback q1';
 	weaken $s;
 	is $commit, 1, 'no commit q1';
+};
+
+
+subtest 'function executed exactly once for no retry' => sub {
+	plan tests => 2;
+	$s = Neo4j::Driver->new({ max_transaction_retry_time => 0 })->plugin($mock)->session;
+	my $run;
+	$r = $s->execute_write(sub { ++$run });
+	is $run, 1, 'scalar context runs once';
+	@r = $s->execute_write(sub { ++$run });
+	is $run, 2, 'list context runs once';
 };
 
 